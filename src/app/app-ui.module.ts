332151c540e652cc9ed9c016c32ebe80606fa73f
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-02 22:10:28 -0400

    added card component

--- src/app/app-ui.module.ts
+++ src/app/app-ui.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 
 const uiModules = [
+  MatCardModule,
   MatSidenavModule,
   MatIconModule,
   MatButtonModule
