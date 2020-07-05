b33a51459a2a5abeeac8f8ebe36da4cd60bc9a88
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-05 15:03:14 -0400

    added contact page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { MembersComponent } from './members/members.component';
@@ -15,6 +16,7 @@ import { PublicationsComponent } from './publications/publications.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'members', component: MembersComponent },
   { path: 'news', component: NewsComponent },
