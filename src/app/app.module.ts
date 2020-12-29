a457cbcdfb7b7fac38dd7d79e5413c8d3343f1ca
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-12-29 00:34:16 -0500

    reorgnized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppUiModule } from './app-ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
-import { PublicationsComponent } from './publications/publications.component';
-import { MembersComponent } from './members/members.component';
-import { FaqComponent } from './faq/faq.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProjectsComponent } from './projects/projects.component';
-import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { MembersComponent } from './members/members.component';
+import { NewsComponent } from './news/news.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PublicationsComponent } from './publications/publications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     AboutComponent,
-    NewsComponent,
-    PublicationsComponent,
-    MembersComponent,
+    ContactComponent,
     FaqComponent,
-    ProjectsComponent,
+    HomeComponent,
+    MembersComponent,
+    NewsComponent,
     PageNotFoundComponent,
-    ContactComponent
+    ProjectsComponent,
+    PublicationsComponent    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    AppUiModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
