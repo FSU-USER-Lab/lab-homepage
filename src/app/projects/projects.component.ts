356b30bcd76ef531d61f1747aa2e6c5b7f0e12b3
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-03-19 23:27:37 -0400

    projects data loaded from json

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import current_projectsData from '../../assets/current_projects.json';
+import past_projectsData from '../../assets/past_projects.json';
+
+interface Project {
+  name: String;
+  date: String;
+  content: String;
+}
 
 @Component({
   selector: 'app-projects',
@@ -12,18 +20,7 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  current_projects = [
-    {
-      title: 'Automating Software Traceability Link Recovery and Maintenance using Word Embeddings within a Neural Network', 
-      date: 'Feb 2020 - Present', 
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Elit ut aliquam purus sit amet luctus.In eu mi bibendum neque egestas congue quisque.Nisi vitae suscipit tellus mauris a diam maecenas sed.Leo duis ut diam quam nulla porttitor massa id neque.Nec feugiat nisl pretium fusce id velit ut.Volutpat diam ut venenatis tellus in.Nisl rhoncus mattis rhoncus urna neque.Diam vulputate ut pharetra sit amet aliquam id diam.'
-    },
-    {
-      title: 'UROP Project #2', 
-      date: "2020", 
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Elit ut aliquam purus sit amet luctus.In eu mi bibendum neque egestas congue quisque.Nisi vitae suscipit tellus mauris a diam maecenas sed.Leo duis ut diam quam nulla porttitor massa id neque.Nec feugiat nisl pretium fusce id velit ut.Volutpat diam ut venenatis tellus in.Nisl rhoncus mattis rhoncus urna neque.Diam vulputate ut pharetra sit amet aliquam id diam.'
-    },
-    
-  ];
+  current_projects: Project[] = current_projectsData;
+  past_projects: Project[] = past_projectsData;
 
 }
